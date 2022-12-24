c0513341d64af5f8a74fc2a277aed7ee903d7add
aakksshhaayy4 <akhareakshay2@gmail.com>
2022-12-25 00:06:52 +0530

    main file

---
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/BirthdayReminder1Application.java
@@ -0,0 +1,17 @@
+package com.codewithakshay.birthdayreminder.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayReminder1Application {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(BirthdayReminder1Application.class, args);
+		} catch (Exception er) {
+			er.printStackTrace();
+		}
+	}
+
+}
