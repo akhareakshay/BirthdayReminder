c2e2aee137bbe95118211109382239f331998c60
aakksshhaayy4 <akhareakshay2@gmail.com>
2022-12-25 00:47:54 +0530

    address entity modifies

--- BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/model/Address.java
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/model/Address.java
@@ -1,10 +1,16 @@
 package com.codewithakshay.birthdayreminder.postgres.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -15,10 +21,18 @@ public class Address {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "address_id")
 	private Long addressId;
+	@NotEmpty(message = "please enter city name")
 	private String city;
+	@NotEmpty(message = "please enter state name")
 	private String state;
+	@NotEmpty(message = "please enter country name")
 	private String country;
+	@NotEmpty(message = "please enter pincode")
 	private String pincode;
+	@UpdateTimestamp
+	@Column(name = "created_at")
+	private Timestamp createdAt;
 
 }
