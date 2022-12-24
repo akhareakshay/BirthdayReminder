e4f7def113701ea8b23f506588ca14c81721cfb3
aakksshhaayy4 <akhareakshay2@gmail.com>
2022-12-25 00:13:40 +0530

    UserManagement repository

---
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/repository/UserManagementRepository.java
@@ -0,0 +1,11 @@
+package com.codewithakshay.birthdayreminder.postgres.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codewithakshay.birthdayreminder.postgres.model.UserManagement;
+
+@Repository
+public interface UserManagementRepository extends JpaRepository<UserManagement, Long>{
+
+}
