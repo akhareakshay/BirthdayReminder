f7eab79c9e2a6f652fc6e0da087be46e6f342d80
aakksshhaayy4 <akhareakshay2@gmail.com>
2022-12-25 02:06:49 +0530

    birthday controller created and saving data into db

---
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/controller/BirthdayController.java
@@ -0,0 +1,43 @@
+package com.codewithakshay.birthdayreminder.postgres.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codewithakshay.birthdayreminder.postgres.model.Birthday;
+import com.codewithakshay.birthdayreminder.postgres.repository.BirthdayRepository;
+
+@RestController
+@RequestMapping("/birthday")
+public class BirthdayController {
+
+	@Autowired
+	private BirthdayRepository birthdayRepository;
+
+	@PostMapping(value = "/saveorupdate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> saveOrUpdateBirthday(@Valid @RequestBody Birthday birthday,
+			BindingResult bindingResult) {
+		try {
+			if (bindingResult.hasErrors()) {
+				List<ObjectError> allErrors = bindingResult.getAllErrors();
+				return new ResponseEntity<>(allErrors, HttpStatus.BAD_REQUEST);
+			}
+			Birthday savedData = birthdayRepository.save(birthday);
+			return new ResponseEntity<>(savedData, HttpStatus.OK);
+		} catch (Exception er) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
