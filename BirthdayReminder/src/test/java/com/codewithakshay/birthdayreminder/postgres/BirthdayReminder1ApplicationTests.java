a182b2c65bf88b5489dc56eaea5263d4eae84de5
Akshay <akhareakshay2@gmail.com>
2022-12-24 23:03:24 +0530

    project structure

---
+++ BirthdayReminder/src/test/java/com/codewithakshay/birthdayreminder/postgres/BirthdayReminder1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithakshay.birthdayreminder.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayReminder1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
