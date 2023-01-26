c1a95342eafdeb289ed04b5a64ca6741b590cae4
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-01-26 15:48:48 +0530

    login controller

---
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.codewithakshay.birthdayreminder.postgres.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codewithakshay.birthdayreminder.postgres.model.Login;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	@PostMapping(value = "/user")
+	public ResponseEntity<Object> loginUser(@RequestBody Login login) {
+		try {
+
+		} catch (Exception er) {
+
+		}
+	}
+
+}
