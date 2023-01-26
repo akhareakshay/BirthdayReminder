575c155e4c8e85719d932b8fec676de50f8157b2
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-01-26 18:45:38 +0530

    user management repository

--- BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/repository/UserManagementRepository.java
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/repository/UserManagementRepository.java
@@ -1,6 +1,7 @@
 package com.codewithakshay.birthdayreminder.postgres.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.codewithakshay.birthdayreminder.postgres.model.UserManagement;
@@ -9,5 +10,8 @@ import com.codewithakshay.birthdayreminder.postgres.model.UserManagement;
 public interface UserManagementRepository extends JpaRepository<UserManagement, Long> {
 
 	public UserManagement findByUserName(String userName);
+
+	@Query("select um.password from UserManagement um where um.userName=:userName")
+	public String findPasswordByUserName(String userName);
 
 }
