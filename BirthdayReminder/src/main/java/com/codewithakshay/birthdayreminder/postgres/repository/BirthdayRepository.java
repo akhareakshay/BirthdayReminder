32a83ff3d3e01e77657a3a1f27d810241ea54401
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-01-26 18:44:51 +0530

    birthday reposutory

--- BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/repository/BirthdayRepository.java
+++ BirthdayReminder/src/main/java/com/codewithakshay/birthdayreminder/postgres/repository/BirthdayRepository.java
@@ -1,11 +1,18 @@
 package com.codewithakshay.birthdayreminder.postgres.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.codewithakshay.birthdayreminder.postgres.model.Birthday;
 
 @Repository
-public interface BirthdayRepository extends JpaRepository<Birthday, Long>{
+public interface BirthdayRepository extends JpaRepository<Birthday, Long> {
+
+//	@Query("select * from Birthday where date like %:date%")
+	public List<Birthday> findByDateLike(String date);
 
 }
